7671f26821a1600cbb1a9ea15b89e588a8067fe1
christmo99 <christmo99@55787118-18e6-c3fa-eee6-1eb2663fe64c>
2010-08-13 19:43:35 +0000

    Funcionalidad Despachos v1 finalizada - sin identificador de llamadas

--- Despachos/src/BaseDatos/prueba.java
+++ Despachos/src/BaseDatos/prueba.java
@@ -18,8 +18,9 @@ public class prueba {
         ResultSet rs = cb.ejecutarConsulta(sql);
         //System.out.println("ID: " + rs.getString(3));
 
-        for (int i=15; i<=20; i++)  {
-            String sql1 = "INSERT INTO CLIENTES(TELEFONO,CODIGO,NOMBRE_APELLIDO_CLI) VALUES('"+(7257917+i)+"','"+i+"','"+"Prueba "+i+"')";
+        for (int i=30; i<=50; i++)  {
+            //String sql1 = "INSERT INTO CLIENTES(TELEFONO,CODIGO,NOMBRE_APELLIDO_CLI) VALUES('"+(7257917+i)+"','"+i+"','"+"Prueba "+i+"')";
+            String sql1 ="INSERT INTO VEHICULOS(PLACA,N_UNIDAD,ID_EMPRESA) VALUES('Placa"+i+"',"+i+",'LN')";
             //System.out.println(""+sql1);
             cb.ejecutarSentencia(sql1);
         }
