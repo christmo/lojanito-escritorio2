c9a5d98e1c114354cdc1df17d24a8218b73fa0b8
christmo99 <christmo99@55787118-18e6-c3fa-eee6-1eb2663fe64c>
2010-09-09 00:03:53 +0000

    Socket para conectar la interfaz web con el escritorio

--- trunk/Despachos/src/interfaz/comunicacion/mapa/SocketMapa.java
+++ trunk/Despachos/src/interfaz/comunicacion/mapa/SocketMapa.java
@@ -53,6 +53,7 @@ public class SocketMapa extends Thread {
         try {
             skServidor.close();
         } catch (IOException ex) {
+            Logger.getLogger(SocketMapa.class.getName()).log(Level.SEVERE, null, ex);
         } catch (NullPointerException ex) {
         }
     }
@@ -63,15 +64,25 @@ public class SocketMapa extends Thread {
             //try {
             System.out.println("Escucho el puerto " + PUERTO);
             Socket skCliente = null;
+
+
             try {
                 skCliente = skServidor.accept();
 
                 //PrintWriter out = new PrintWriter(skCliente.getOutputStream(), true);
                 //PrintWriter out = new PrintWriter(skCliente.getOutputStream(), true);
+
+
                 int i = -1;
                 String datosMapa = "";
+
+
                 int contarSeparador = 0;
+
+
                 char caracter;
+
+
                 while (escuchar) {
 
                     is = skCliente.getInputStream();
@@ -81,25 +92,32 @@ public class SocketMapa extends Thread {
                     //if (i != -1) {
                     caracter = (char) i;
                     //System.out.println("Code: " + i);
+
+
                     if (i != 10) {
                         if (caracter == '%') {
                             contarSeparador++;
+
+
                         }
                         if (contarSeparador == 2) {
                             System.out.println("Coordenadas: " + datosMapa);
                             procesarCoordenadas(datosMapa);
                             //escuchar = false;
+
+
                         } else {
                             datosMapa += "" + caracter;
                             escuchar = true;
+
+
                         }
                     } else {
                         datosMapa = "";
                         contarSeparador = 0;
                         CerrarPuerto();
                         new SocketMapa();
-                    }
-                    //}
+                    } //}
 
                 }
             } catch (SocketException se) {
@@ -109,16 +127,23 @@ public class SocketMapa extends Thread {
         } catch (IOException ex) {
             Logger.getLogger(SocketMapa.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+
     }
 
     private void procesarCoordenadas(String datos) {
         String[] coord = datos.split("%");
         //System.out.println("Lat: " + coord[1]);
         //System.out.println("Lon: " + coord[0]);
+
         try {
             VentanaDatos.setCoordenadasMapa(coord[1], coord[0]);
+
+
         } catch (NullPointerException ex) {
             System.out.println("No hay ventana de datos...");
+
+
         }
     }
 
