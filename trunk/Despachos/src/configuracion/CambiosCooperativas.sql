08c4d48ade9cc5afb9c0fa13df8dc79667d52c7d
christmo99 <christmo99@55787118-18e6-c3fa-eee6-1eb2663fe64c>
2010-12-06 18:09:28 +0000

    Captura de una excepción en la interfaz principal y cambios en la base de datos

--- trunk/Despachos/src/configuracion/CambiosCooperativas.sql
+++ trunk/Despachos/src/configuracion/CambiosCooperativas.sql
@@ -31,6 +31,7 @@ ADD DIRECCION VARCHAR(100) NOT NULL AFTER USUARIO;
 ALTER TABLE server
 ADD DIRECCION VARCHAR(100) NOT NULL AFTER USUARIO;
 
+DROP TABLE IF EXISTS `server`;
 create table server(
   n_unidad int(12),
   cod_cliente int(11),
@@ -44,9 +45,10 @@ create table server(
   direccion varchar(100) NOT NULL
 )
 ENGINE=FEDERATED
-CONNECTION='mysql://USER:PASSWD@200.0.29.121:3306/rastreosatelital/TABLA';
+CONNECTION='mysql://riveralcazar:1A9l4c9A7r5i2v0E1r7@200.0.29.121:3306/rastreosatelital/ASIGNADOS_RA';
 
-CREATE TABLE  `rastreo`.`respaldo_asignacion_server` (
+DROP TABLE IF EXISTS `respaldo_asignacion_server`;
+CREATE TABLE  `respaldo_asignacion_server` (
   `N_UNIDAD` int(12) NOT NULL,
   `COD_CLIENTE` int(11) DEFAULT NULL,
   `ESTADO` varchar(25) NOT NULL,
@@ -57,3 +59,37 @@ CREATE TABLE  `rastreo`.`respaldo_asignacion_server` (
   `USUARIO` varchar(25) NOT NULL,
   `DIRECCION` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+ALTER TABLE VEHICULOS
+ADD USUARIO VARCHAR(120) DEFAULT NULL AFTER NUM_CHASIS,
+ADD REG_MUNICIPAL INT(5) DEFAULT NULL AFTER USUARIO;
+
+DROP TABLE IF EXISTS `posicion_clientes`;
+CREATE TABLE `posicion_clientes` (
+  `COD_CLIENTE` int(11) NOT NULL,
+  `N_UNIDAD` int(11) DEFAULT NULL,
+  `NOMBRE` varchar(255) DEFAULT NULL,
+  `BARRIO` varchar(125) DEFAULT NULL,
+  `FONO` varchar(10) DEFAULT NULL,
+  `LATITUD` double NOT NULL,
+  `LONGITUD` double NOT NULL,
+  `FECHA` date NOT NULL,
+  `HORA` time NOT NULL,
+  PRIMARY KEY (`COD_CLIENTE`,`LATITUD`,`LONGITUD`,`FECHA`,`HORA`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `recorridos`;
+CREATE TABLE  `recorridos` (
+  `ID` int(12) DEFAULT NULL,
+  `N_UNIDAD` int(11) NOT NULL,
+  `ID_EMPRESA` varchar(10) NOT NULL,
+  `LATITUD` double DEFAULT NULL,
+  `LONGITUD` double DEFAULT NULL,
+  `FECHA` date NOT NULL,
+  `HORA` time NOT NULL,
+  `VELOCIDAD` double DEFAULT NULL,
+  `G1` int(1) unsigned DEFAULT NULL,
+  `G2` int(1) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC
+PARTITION BY HASH (ID)
+PARTITIONS 1024;
