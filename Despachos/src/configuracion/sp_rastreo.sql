5045965148f206cf3774495ff22bd3415d0b303b
christmo99 <christmo99@55787118-18e6-c3fa-eee6-1eb2663fe64c>
2010-12-09 23:05:40 +0000

    actualización de la base de datos, cambio en el archivo para la configuración del comando del modem en algunas cooperativas

--- Despachos/src/configuracion/sp_rastreo.sql
+++ Despachos/src/configuracion/sp_rastreo.sql
@@ -1,8 +1,3 @@
----------------------------------------------------------
--- christmo
----------------------------------------------------------
--- Obtenida con el comando: mysqldump -u root -p -R -d rastreo > rastreo.sql
----------------------------------------------------------
 -- MySQL dump 10.13  Distrib 5.1.36, for Win32 (ia32)
 --
 -- Host: localhost    Database: rastreo
@@ -19,374 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `accesos`
---
-
-DROP TABLE IF EXISTS `accesos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `accesos` (
-  `IP` varchar(255) DEFAULT NULL,
-  `HOST` varchar(255) DEFAULT NULL,
-  `USUARIO` varchar(255) DEFAULT NULL,
-  `FECHA` date DEFAULT NULL,
-  `HORA` time DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `asignados`
---
-
-DROP TABLE IF EXISTS `asignados`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `asignados` (
-  `ID_ASIGNADOS` int(11) NOT NULL AUTO_INCREMENT,
-  `COD_CLIENTE` int(11) DEFAULT NULL,
-  `FECHA` date NOT NULL,
-  `HORA` time NOT NULL,
-  `TELEFONO` varchar(25) NOT NULL,
-  `NOMBRE_APELLIDO_CLI` varchar(125) DEFAULT NULL,
-  `DIRECCION_CLI` varchar(125) DEFAULT NULL,
-  `SECTOR` varchar(125) DEFAULT NULL,
-  `MINUTOS` int(11) NOT NULL,
-  `N_UNIDAD` int(11) DEFAULT NULL,
-  `ATRASO` int(11) NOT NULL,
-  `NOTA` varchar(255) DEFAULT NULL,
-  `ESTADO` varchar(5) DEFAULT NULL,
-  `ID_TURNO` int(11) DEFAULT NULL,
-  `USUARIO` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`ID_ASIGNADOS`)
-) ENGINE=MyISAM AUTO_INCREMENT=7468 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `centrales`
---
-
-DROP TABLE IF EXISTS `centrales`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `centrales` (
-  `ID_EMPRESA` varchar(10) DEFAULT NULL,
-  `LATITUD` double DEFAULT NULL,
-  `LONGITUD` double DEFAULT NULL,
-  `DIRECCION` varchar(150) DEFAULT NULL,
-  `TELEFONO` varchar(150) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `clientes`
---
-
-DROP TABLE IF EXISTS `clientes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `clientes` (
-  `TELEFONO` varchar(25) NOT NULL DEFAULT '',
-  `CODIGO` int(10) DEFAULT NULL,
-  `NOMBRE_APELLIDO_CLI` varchar(125) NOT NULL,
-  `DIRECCION_CLI` varchar(125) NOT NULL,
-  `SECTOR` varchar(125) DEFAULT NULL,
-  `NUM_CASA_CLI` varchar(10) DEFAULT NULL,
-  `LATITUD` double DEFAULT NULL,
-  `LONGITUD` double DEFAULT NULL,
-  `INFOR_ADICIONAL` varchar(225) DEFAULT NULL,
-  PRIMARY KEY (`NOMBRE_APELLIDO_CLI`,`DIRECCION_CLI`,`TELEFONO`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `cod_multas`
---
-
-DROP TABLE IF EXISTS `cod_multas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cod_multas` (
-  `IDENTIFICADOR` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `COD_MULTA` varchar(5) NOT NULL,
-  `DESCRIPCION` varchar(255) DEFAULT NULL,
-  `VALOR` double DEFAULT NULL,
-  PRIMARY KEY (`IDENTIFICADOR`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `codesttaxi`
---
-
-DROP TABLE IF EXISTS `codesttaxi`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `codesttaxi` (
-  `ID_CODIGO` varchar(50) NOT NULL,
-  `ETIQUETA` varchar(255) DEFAULT NULL,
-  `COLOR` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`ID_CODIGO`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `conductores`
---
-
-DROP TABLE IF EXISTS `conductores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `conductores` (
-  `ID_CON` int(11) NOT NULL AUTO_INCREMENT,
-  `CEDULA_CONDUCTOR` varchar(10) NOT NULL,
-  `NOMBRE_APELLIDO_CON` varchar(125) NOT NULL,
-  `DIRECCION_CON` varchar(125) NOT NULL,
-  `NUM_CASA_CON` varchar(10) DEFAULT NULL,
-  `TIPO_SANGRE` varchar(10) DEFAULT NULL,
-  `ESTADO_CIVIL` varchar(25) DEFAULT NULL,
-  `CONYUGE` varchar(125) DEFAULT NULL,
-  `MAIL` varchar(125) DEFAULT NULL,
-  `FOTO` varchar(125) DEFAULT NULL,
-  PRIMARY KEY (`ID_CON`,`CEDULA_CONDUCTOR`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=80 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `empresas`
---
-
-DROP TABLE IF EXISTS `empresas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `empresas` (
-  `ID_EMPRESA` varchar(10) NOT NULL,
-  `NOMBRE_EMP` varchar(100) NOT NULL,
-  `MODEM` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`ID_EMPRESA`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `multas`
---
-
-DROP TABLE IF EXISTS `multas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `multas` (
-  `USUARIO` varchar(100) DEFAULT NULL,
-  `N_UNIDAD` int(11) DEFAULT NULL,
-  `FECHA` date DEFAULT NULL,
-  `HORA` time DEFAULT NULL,
-  `COD_MULTA` varchar(5) DEFAULT NULL,
-  `ESTADO` int(1) DEFAULT NULL,
-  KEY `FK_REFERENCE_13` (`COD_MULTA`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `multas_asignadas`
---
-
-DROP TABLE IF EXISTS `multas_asignadas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `multas_asignadas` (
-  `ID_ASIG` int(10) NOT NULL AUTO_INCREMENT,
-  `USUARIO` varchar(100) DEFAULT NULL,
-  `N_UNIDAD` int(11) DEFAULT NULL,
-  `FECHA` date DEFAULT NULL,
-  `HORA` time DEFAULT NULL,
-  `COD_MULTA` varchar(5) DEFAULT NULL,
-  PRIMARY KEY (`ID_ASIG`),
-  KEY `FK_CODMULTAS` (`COD_MULTA`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `multas_pagadas`
---
-
-DROP TABLE IF EXISTS `multas_pagadas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `multas_pagadas` (
-  `ID_ASIG` int(10) DEFAULT NULL,
-  `USUARIO` varchar(100) DEFAULT NULL,
-  `FECHA` date DEFAULT NULL,
-  `HORA` time DEFAULT NULL,
-  KEY `FK_REFERENCE_14` (`ID_ASIG`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `posicion_clientes`
---
-
-DROP TABLE IF EXISTS `posicion_clientes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `posicion_clientes` (
-  `COD_CLIENTE` int(11) NOT NULL,
-  `N_UNIDAD` int(11) DEFAULT NULL,
-  `NOMBRE` varchar(255) DEFAULT NULL,
-  `BARRIO` varchar(125) DEFAULT NULL,
-  `FONO` varchar(10) DEFAULT NULL,
-  `LATITUD` double NOT NULL,
-  `LONGITUD` double NOT NULL,
-  `FECHA` date NOT NULL,
-  `HORA` time NOT NULL,
-  PRIMARY KEY (`COD_CLIENTE`,`LATITUD`,`LONGITUD`,`FECHA`,`HORA`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `recorridos`
---
-
-DROP TABLE IF EXISTS `recorridos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  `recorridos` (
-  `ID` int(12) DEFAULT NULL,
-  `N_UNIDAD` int(11) NOT NULL,
-  `ID_EMPRESA` varchar(10) NOT NULL,
-  `LATITUD` double DEFAULT NULL,
-  `LONGITUD` double DEFAULT NULL,
-  `FECHA` date NOT NULL,
-  `HORA` time NOT NULL,
-  `VELOCIDAD` double DEFAULT NULL,
-  `G1` int(1) unsigned DEFAULT NULL,
-  `G2` int(1) unsigned DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC
-PARTITION BY HASH (ID)
-PARTITIONS 1024;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `regcodesttaxi`
---
-
-DROP TABLE IF EXISTS `regcodesttaxi`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `regcodesttaxi` (
-  `FECHA` date NOT NULL,
-  `HORA` time NOT NULL,
-  `ID_CODIGO` varchar(50) DEFAULT NULL,
-  `USUARIO` varchar(100) DEFAULT NULL,
-  `N_UNIDAD` int(11) NOT NULL,
-  PRIMARY KEY (`FECHA`,`HORA`,`N_UNIDAD`),
-  KEY `FK_REFERENCE_10` (`ID_CODIGO`),
-  KEY `FK_REFERENCE_12` (`USUARIO`),
-  KEY `FK_N_UNIDAD` (`N_UNIDAD`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `respaldo_asignacion_server`
---
-
-DROP TABLE IF EXISTS `respaldo_asignacion_server`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `respaldo_asignacion_server` (
-  `N_UNIDAD` int(12) NOT NULL,
-  `COD_CLIENTE` int(11) DEFAULT NULL,
-  `ESTADO` varchar(25) NOT NULL,
-  `FECHA` date NOT NULL,
-  `HORA` int(11) NOT NULL,
-  `FONO` varchar(25) DEFAULT NULL,
-  `HORA_INSERT` bigint(20) NOT NULL,
-  `USUARIO` varchar(25) NOT NULL,
-  `DIRECCION` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `telefonos`
---
-
-DROP TABLE IF EXISTS `telefonos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `telefonos` (
-  `ID_TELEFONO` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ID_CON` int(11) DEFAULT NULL,
-  `NUMERO` varchar(25) DEFAULT NULL,
-  `TIPO` varchar(25) DEFAULT NULL,
-  PRIMARY KEY (`ID_TELEFONO`),
-  KEY `FK_TIENEN` (`ID_CON`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `turnos`
---
-
-DROP TABLE IF EXISTS `turnos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `turnos` (
-  `ID_TURNO` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `HORA_INI` time DEFAULT NULL,
-  `HORA_FIN` time DEFAULT NULL,
-  PRIMARY KEY (`ID_TURNO`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuarios` (
-  `ID_EMPRESA` varchar(10) NOT NULL,
-  `USUARIO` varchar(100) NOT NULL,
-  `CLAVE` varchar(255) NOT NULL,
-  `NOMBRE_USUARIO` varchar(100) DEFAULT NULL,
-  `DIRECCION` varchar(255) DEFAULT NULL,
-  `TELEFONO` varchar(255) DEFAULT NULL,
-  `ID_TURNO` int(10) DEFAULT NULL,
-  `ESTADO` varchar(255) DEFAULT NULL,
-  `OPERADOR` varchar(255) DEFAULT NULL,
-  `CEDULA_USUARIO` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`ID_EMPRESA`,`USUARIO`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `vehiculos`
---
-
-DROP TABLE IF EXISTS `vehiculos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `vehiculos` (
-  `PLACA` varchar(10) NOT NULL,
-  `N_UNIDAD` int(11) NOT NULL,
-  `ID_EMPRESA` varchar(10) NOT NULL,
-  `ID_CON` int(11) DEFAULT NULL,
-  `CONDUCTOR_AUX` int(11) DEFAULT NULL,
-  `MODELO` varchar(25) DEFAULT NULL,
-  `ANIO` int(5) DEFAULT NULL,
-  `PROPIETARIO` varchar(125) DEFAULT NULL,
-  `INF_ADICIONAL` varchar(255) DEFAULT NULL,
-  `IMAGEN` varchar(125) DEFAULT NULL,
-  `MARCA` varchar(45) DEFAULT NULL,
-  `NUM_MOTOR` varchar(125) DEFAULT NULL,
-  `NUM_CHASIS` varchar(125) DEFAULT NULL,
-  `USUARIO` varchar(125) DEFAULT NULL,
-  `REG_MUNICIPAL` int(5) DEFAULT NULL,
-  PRIMARY KEY (`PLACA`,`N_UNIDAD`,`ID_EMPRESA`),
-  KEY `FK_REFERENCE_6` (`ID_EMPRESA`),
-  KEY `FK_REFERENCE_8` (`ID_CON`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping routines for database 'rastreo'
@@ -461,7 +88,6 @@ HORA =(
 	WHERE N_UNIDAD=UNIDAD AND FECHA = (SELECT MAX(FECHA) FROM REGCODESTTAXI WHERE N_UNIDAD=UNIDAD)
 	GROUP BY FECHA
 );
-
 RETURN ESTADO;
 
 END */;;
@@ -541,8 +167,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 PROCEDURE `SP_ACTUALIZAR_USUARIOS`(
-IN PID_EMPRESA varchar(10),
+/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 PROCEDURE `SP_ACTUALIZAR_USUARIOS`(IN PID_EMPRESA varchar(10),
 IN PUSUARIO varchar(100),
 IN PCLAVE varchar(255),
 IN PNOMBRE_USUARIO VARCHAR(100),
@@ -649,23 +274,25 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 PROCEDURE `SP_INSERTAR_RECORRIDOS`(
-IN PID_PART varchar(10),
-IN PN_UNIDAD INTEGER,
-IN PID_EMPRESA varchar(10),
-IN PLAT DOUBLE,
-IN PLON DOUBLE,
-IN PFECHA DATE,
-IN PHORA TIME,
-IN PES_TAXI VARCHAR(25),
-IN PVEL DOUBLE,
-IN PES_TAXIM CHAR(10)
+/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 PROCEDURE `SP_INSERTAR_RECORRIDOS`(
+IN PID_PART varchar(10),
+IN PN_UNIDAD INTEGER,
+IN PID_EMPRESA varchar(10),
+IN PLAT DOUBLE,
+IN PLON DOUBLE,
+IN PFECHA DATE,
+IN PHORA TIME,
+IN PVEL VARCHAR(25),
+IN PG1 DOUBLE,
+IN PG2 CHAR(10)
 )
-BEGIN
-
-INSERT INTO RECORRIDOS(N_UNIDAD, ID_EMPRESA,LATITUD,LONGITUD,FECHA,HORA,EST_TAXI,VELOCIDAD,EST_TAXIM)
-VALUES (PN_UNIDAD,PID_EMPRESA,PLAT,PLON,PFECHA,PHORA,PES_TAXI,PVEL,PES_TAXIM);
-
+BEGIN
+
+/*INSERT INTO RECORRIDOS(N_UNIDAD, ID_EMPRESA,LATITUD,LONGITUD,FECHA,HORA,EST_TAXI,VELOCIDAD,EST_TAXIM)
+VALUES (PN_UNIDAD,PID_EMPRESA,PLAT,PLON,PFECHA,PHORA,PES_TAXI,PVEL,PES_TAXIM);*/
+
+INSERT INTO RECORRIDOS(ID,N_UNIDAD, ID_EMPRESA,LATITUD,LONGITUD,FECHA,HORA,VELOCIDAD,G1,G2)
+VALUES (PID_PART,PN_UNIDAD,PID_EMPRESA,PLAT,PLON,PFECHA,PHORA,PVEL,PG1,PG2);
 END */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -682,8 +309,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 PROCEDURE `SP_INSERTAR_USUARIOS`(
-IN PID_EMPRESA varchar(10),
+/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 PROCEDURE `SP_INSERTAR_USUARIOS`(IN PID_EMPRESA varchar(10),
 IN PUSUARIO varchar(100),
 IN PCLAVE varchar(255),
 IN PNOMBRE_USUARIO VARCHAR(100),
@@ -698,7 +324,6 @@ BEGIN
 
 INSERT INTO USUARIOS
 VALUES (PID_EMPRESA,PUSUARIO,PCLAVE,PNOMBRE_USUARIO,PDIRECCION,PTELEFONO,PID_TURNO,PESTADO,POPERADOR,PCI);
-
 END */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -730,8 +355,7 @@ IN MAR VARCHAR(45),
 IN MOT VARCHAR(125),
 IN CHA VARCHAR(125),
 IN IDUSER VARCHAR(100),
-IN REG_MUN int(5)
-)
+IN REG_MUN int(5))
 BEGIN
 
 DECLARE IDCON INT(11);
@@ -769,6 +393,7 @@ REG_MUN
 
 INSERT INTO REGCODESTTAXI
 VALUES (NOW(),NOW(),'AC',IDUSER,NUNI);
+
 
 END */;;
 DELIMITER ;
@@ -851,4 +476,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-11-19 19:25:46
+-- Dump completed on 2010-12-09 15:27:28
---
+++ Despachos/src/configuracion/tabla_servidor.sql
@@ -0,0 +1,46 @@
+/*
+ * Cambios en la cooperativas para que se igualen las base de datos
+ * christmo ;-)
+ */
+
+/*
+Configuracion MySQL
+[mysqld]
+port=3306
+federated
+long_query_time = 5
+bind-address = 0.0.0.0
+
+consultar para ver si federated esta activado
+show engines;
+
+-----------------------------------------------------------------------------
+COOP                USUARIO         CLAVE                       TABLA
+LojaNorte           lojanort        l3O4j1a7n3O0r8t13           ASIGNADOS_LN
+Ciudad Victoria     cityvictory     C5i9t1Y0V4i7c9t0O2r7y	ASIGNADOS_CV
+Riveras de Alcazar  riveralcazar    1A9l4c9A7r5i2v0E1r7         ASIGNADOS_RA
+TransPeñaSur        trpesur         9s4U89p3E0r2t5Ru6a3N9s0	ASIGNADOS_TP
+-----------------------------------------------------------------------------
+
+*/
+
+--
+-- Table structure for table `server`
+--
+
+DROP TABLE IF EXISTS `server`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `server` (
+  `n_unidad` int(12) DEFAULT NULL,
+  `cod_cliente` int(11) DEFAULT NULL,
+  `estado` varchar(25) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `fono` varchar(25) NOT NULL,
+  `valor` int(12) DEFAULT NULL,
+  `estado_insert` varchar(3) DEFAULT NULL,
+  `usuario` varchar(25) DEFAULT NULL,
+  `direccion` varchar(100) NOT NULL
+) ENGINE=FEDERATED DEFAULT CHARSET=latin1 CONNECTION='mysql://usuario:clave@200.0.29.121:3306/rastreosatelital/TABLA';
+/*!40101 SET character_set_client = @saved_cs_client */;
