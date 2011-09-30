b677b1f4eb672e7562a8b4fffb2b11fe77b97649
christmo99 <christmo99@55787118-18e6-c3fa-eee6-1eb2663fe64c>
2011-09-30 22:14:56 +0000

    swingworker para el actualizador de respaldos

--- trunk/Despachos/src/interfaz/comunicacion/servidorBD/ActualizadorDespachosServidorKRADAC.java
+++ trunk/Despachos/src/interfaz/comunicacion/servidorBD/ActualizadorDespachosServidorKRADAC.java
@@ -1,5 +1,6 @@
 package interfaz.comunicacion.servidorBD;
 
+import javax.swing.SwingWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sistemaoperativo.ComandosSistemaOperativo;
@@ -22,14 +23,22 @@ public class ActualizadorDespachosServidorKRADAC extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-            try {
-                comando = "java -jar ActualizarServidor.jar " + empresa;
-                ComandosSistemaOperativo cmd = new ComandosSistemaOperativo(comando);
-                ActualizadorDespachosServidorKRADAC.sleep(3600000);//60 minutos
-            } catch (InterruptedException ex) {
-                log.info("{}", ex.getMessage(), ex);
+        SwingWorker hiloIndependiente = new SwingWorker() {
+
+            @Override
+            protected Object doInBackground() throws Exception {
+                while (true) {
+                    try {
+                        comando = "java -jar ActualizarServidor.jar " + empresa;
+                        ComandosSistemaOperativo cmd = new ComandosSistemaOperativo(comando);
+                        ActualizadorDespachosServidorKRADAC.sleep(3600000);//60 minutos
+                    } catch (InterruptedException ex) {
+                        log.info("{}", ex.getMessage(), ex);
+                    }
+                }
             }
-        }
+        };
+
+        hiloIndependiente.execute();
     }
 }
