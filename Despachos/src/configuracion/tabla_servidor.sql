028c3b8950b07cdc6ceda73cb63490ae7d25cc40
christmo99 <christmo99@55787118-18e6-c3fa-eee6-1eb2663fe64c>
2012-03-16 20:05:23 +0000

    Scripts y cambios en la base

--- Despachos/src/configuracion/tabla_servidor.sql
+++ Despachos/src/configuracion/tabla_servidor.sql
@@ -23,10 +23,13 @@ TransPeñaSur        trpesur	9s4U89p3E0r2t5Ru6a3N9s0             ASIGNADOS_TP
 Andina Sur          adnsur	c827dc4d8976H962505f9288            ASIGNADOS_AS
 Taxopel             taxopel	336c2cb0b84800172f6e5dfcc570faeb    ASIGNADOS_TX
 Loja Turistica      lojaturist	f9817fc5cbb25fc9d7da53e1dfc14cd3    ASIGNADOS_LT
+DiscaTaxi           discataxi	N3g13r20dsk199tx38k2r3c             ASIGNADOS_DT
 Kradac              krctest	krctesting                          ASIGNADOS_KR
 -----------------------------------------------------------------------------
 
 */
+
+use rastreosatelital;
 
 --
 -- Table structure for table `server_federada`
@@ -132,6 +135,20 @@ CREATE TABLE `server_federada` (
   `usuario` varchar(25) DEFAULT NULL,
   `direccion` varchar(150) DEFAULT NULL
 ) ENGINE=FEDERATED DEFAULT CHARSET=latin1 CONNECTION='mysql://lojanort:l3O4j1a7n3O0r8t13@200.0.29.121:3306/rastreosatelital/ASIGNADOS_LN';
+
+/*DISCATAXI*/
+CREATE TABLE `server_federada` (
+  `n_unidad` int(12) DEFAULT NULL,
+  `cod_cliente` int(11) DEFAULT NULL,
+  `estado` varchar(25) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `fono` varchar(25) DEFAULT NULL,
+  `valor` int(12) DEFAULT NULL,
+  `estado_insert` varchar(3) DEFAULT NULL,
+  `usuario` varchar(25) DEFAULT NULL,
+  `direccion` varchar(150) DEFAULT NULL
+) ENGINE=FEDERATED DEFAULT CHARSET=latin1 CONNECTION='mysql://discataxi:N3g13r20dsk199tx38k2r3c@200.0.29.121:3306/rastreosatelital/ASIGNADOS_DT';
 
 /*
 CREATE TABLE `server_federada` (
